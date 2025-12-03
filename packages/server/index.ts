095a4ea3b83beabb0b4cc80a257e0cb29da685ca
ron45700 <ron.mintz21@gmail.com>
2025-12-03 14:33:17 +0200

    create the frontend

---
+++ packages/server/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import type {Request , Response} from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res:Response) => {
+  //res.send(process.env.OPENAI_API_KEY);
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
