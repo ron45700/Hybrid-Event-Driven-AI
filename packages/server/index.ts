c525be2b6aa0abea7bc9d113149f63c7cac4b434
ron45700 <ron.mintz21@gmail.com>
2025-12-03 15:04:31 +0200

    connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,9 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res:Response) => {
-  //res.send(process.env.OPENAI_API_KEY);
   res.send('Hello, World!');
 });
+
+app.get('/api/hello', (req: Request, res:Response) => {
+  res.json ({ message: 'Hello World!' });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
